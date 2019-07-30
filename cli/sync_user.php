0a7e9329323e35606b1f7d3d1e3f88dec7c2b45e
David Thompson <david.thompson@catalyst.net.nz>
2019-07-30 12:02:54 +1200

    Added CLI sync scripts (still need cohort creation perms).

---
+++ cli/sync_user.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CLI script: synchronise cohorts for a single user.
+ * @package    local_cohortauto
+ * @copyright  2019 Catalyst IT
+ * @author     David Thompson <david.thompson@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__.'/../../../config.php');
+require_once($CFG->libdir . '/clilib.php');
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->dirroot . '/local/cohortauto/lib.php');
+
+// TODO: find a way to assert permission to create cohorts.
+
+$username = cli_input('Synchronise cohorts for username:');
+
+if ($username) {
+    if ($user = $DB->get_record('user', array('username' => $username))) {
+        $handler = new local_cohortauto_handler();
+        $handler->user_profile_hook($user);
+
+        cli_writeln("Sync for user '$username' complete.");
+    } else {
+        cli_error("User '$username' not found.");
+    }
+}
---
+++ cli/sync_users.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CLI script: synchronise cohorts for all users.
+ * @package    local_cohortauto
+ * @copyright  2019 Catalyst IT
+ * @author     David Thompson <david.thompson@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__.'/../../../config.php');
+require_once($CFG->libdir . "/clilib.php");
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->dirroot . '/local/cohortauto/lib.php');
+
+// TODO: find a way to assert permission to create cohorts.
+
+$handler = new local_cohortauto_handler();
+$users = $DB->get_recordset('user', array('deleted' => 0));
+
+cli_writeln("Beginning user cohort sync...");
+$transaction = $DB->start_delegated_transaction();
+
+foreach ($users as $user) {
+    $username = $user->username;
+    cli_write("  User '{$username}' ... ");
+    $handler->user_profile_hook($user);
+    cli_writeln("done.");
+}
+$users->close();
+
+$transaction->allow_commit();
+cli_writeln("Sync finished.");
