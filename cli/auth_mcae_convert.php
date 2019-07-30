87259ea75a6b3eef026aed26ecabc177cb10d01a
David Thompson <david.thompson@catalyst.net.nz>
2019-07-30 17:22:42 +1200

    Added 'auth_mcae_convert' CLI script.

---
+++ cli/auth_mcae_convert.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CLI script: convert all auth_mcae cohorts to local_cohortauto cohorts.
+ * @package    local_cohortauto
+ * @copyright  2019 Catalyst IT
+ * @author     David Thompson <david.thompson@catalyst.net.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__.'/../../../config.php');
+
+$updatesql = "UPDATE {cohort}
+                 SET component = 'local_cohortauto'
+               WHERE component = 'auth_mcae'";
+
+$DB->execute($updatesql);
+
+cli_writeln("Migrated any auth_mcae cohorts to local_cohortauto.");
