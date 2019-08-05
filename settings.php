efd703969b1292c2f13280da3f45c75bbe8f63c6
David Thompson <david.thompson@catalyst.net.nz>
2019-08-06 11:42:53 +1200

    settings.php: admintree fix: moved fullpage block into $hassiteconfig

--- settings.php
+++ settings.php
@@ -39,71 +39,71 @@ if ($hassiteconfig) { // Needs this condition or there is error on login page.
     $ADMIN->add('accounts', new admin_externalpage('cohortautoview',
         get_string('label_cohortautoview', 'local_cohortauto'),
         new moodle_url('/local/cohortauto/view.php')));
-}
 
-if ($ADMIN->fulltree) {
+    if ($ADMIN->fulltree) {
 
-    $settings = new admin_settingpage('local_cohortauto',
-        get_string('pluginname', 'local_cohortauto'));
-    $ADMIN->add('localplugins', $settings);
+        $settings = new admin_settingpage('local_cohortauto',
+            get_string('pluginname', 'local_cohortauto'));
 
-    // Profile field helper.
-    $fldlist = array();
-    $usrhelper = get_admin();
+        // Profile field helper.
+        $fldlist = array();
+        $usrhelper = get_admin();
 
-    profile_load_data($usrhelper);
-    profile_load_custom_fields($usrhelper);
-    $fldlist = cohortauto_prepare_profile_data($usrhelper);
+        profile_load_data($usrhelper);
+        profile_load_custom_fields($usrhelper);
+        $fldlist = cohortauto_prepare_profile_data($usrhelper);
 
-    // Additional values for email.
-    if (!empty($fldlist['email'])) {
-        $fldlist['email'] = array(
-            'full' => 'exampleuser@mail.example.com',
-            'username' => 'exampleuser',
-            'domain' => 'mail.example.com',
-            'rootdomain' => 'example.com'
+        // Additional values for email.
+        if (!empty($fldlist['email'])) {
+            $fldlist['email'] = array(
+                'full' => 'exampleuser@mail.example.com',
+                'username' => 'exampleuser',
+                'domain' => 'mail.example.com',
+                'rootdomain' => 'example.com'
+            );
+        }
+
+        $helparray = array();
+        cohortauto_print_profile_data($fldlist, '', $helparray);
+
+        $helptext = implode(', ', $helparray);
+        $settings->add(new admin_setting_heading(
+            'local_cohortauto_profile_help',
+            get_string('profile_help', 'local_cohortauto'),
+            $helptext)
         );
+        $settings->add(new admin_setting_configtextarea(
+            'local_cohortauto/mainrule_fld',
+            get_string('mainrule_fld', 'local_cohortauto'),
+            '', '')
+        );
+        $settings->add(new admin_setting_configselect(
+            'local_cohortauto/delim',
+            get_string('delim', 'local_cohortauto'),
+            get_string('delim_help', 'local_cohortauto'),
+            'CR+LF',
+            array('CR+LF' => 'CR+LF', 'CR' => 'CR', 'LF' => 'LF'))
+        );
+        $settings->add(new admin_setting_configtext(
+            'local_cohortauto/secondrule_fld',
+            get_string('secondrule_fld', 'local_cohortauto'),
+            '', 'n/a')
+        );
+        $settings->add(new admin_setting_configtextarea(
+            'local_cohortauto/replace_arr',
+            get_string('replace_arr', 'local_cohortauto'),
+            '', '')
+        );
+        $settings->add(new admin_setting_configtextarea(
+            'local_cohortauto/donttouchusers',
+            get_string('donttouchusers', 'local_cohortauto'),
+            '', '')
+        );
+        $settings->add(new admin_setting_configcheckbox(
+            'local_cohortauto/enableunenrol',
+            get_string('enableunenrol', 'local_cohortauto'),
+            '', 0)
+        );
+        $ADMIN->add('localplugins', $settings);
     }
-
-    $helparray = array();
-    cohortauto_print_profile_data($fldlist, '', $helparray);
-
-    $helptext = implode(', ', $helparray);
-    $settings->add(new admin_setting_heading(
-        'local_cohortauto_profile_help',
-        get_string('profile_help', 'local_cohortauto'),
-        $helptext)
-    );
-    $settings->add(new admin_setting_configtextarea(
-        'local_cohortauto/mainrule_fld',
-        get_string('mainrule_fld', 'local_cohortauto'),
-        '', '')
-    );
-    $settings->add(new admin_setting_configselect(
-        'local_cohortauto/delim',
-        get_string('delim', 'local_cohortauto'),
-        get_string('delim_help', 'local_cohortauto'),
-        'CR+LF',
-        array('CR+LF' => 'CR+LF', 'CR' => 'CR', 'LF' => 'LF'))
-    );
-    $settings->add(new admin_setting_configtext(
-        'local_cohortauto/secondrule_fld',
-        get_string('secondrule_fld', 'local_cohortauto'),
-        '', 'n/a')
-    );
-    $settings->add(new admin_setting_configtextarea(
-        'local_cohortauto/replace_arr',
-        get_string('replace_arr', 'local_cohortauto'),
-        '', '')
-    );
-    $settings->add(new admin_setting_configtextarea(
-        'local_cohortauto/donttouchusers',
-        get_string('donttouchusers', 'local_cohortauto'),
-        '', '')
-    );
-    $settings->add(new admin_setting_configcheckbox(
-        'local_cohortauto/enableunenrol',
-        get_string('enableunenrol', 'local_cohortauto'),
-        '', 0)
-    );
 }
